edda6073b3bff5b9f639bcb109f92c8943dc7b93
Joy43 <ssjoy43@gmail.com>
2025-06-04 16:49:29 +0600

    enhance Toaster component with custom styles and options for improved user notifications

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,22 @@ createRoot(document.getElementById('root')!).render(
       <PersistGate loading={null} persistor={persistor}>
         <RouterProvider router={router} />
       </PersistGate>
-      <Toaster className='items-center ' />
+      <Toaster
+        position="top-right" 
+        richColors 
+        closeButton 
+        toastOptions={{
+          duration: 3000, 
+          style: {
+            fontSize: '16px',
+            padding: '12px 16px',
+            borderRadius: '12px',
+            background: '#fff',
+            color: '#333',
+            boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+          },
+        }}
+      />
     </Provider>
   </StrictMode>,
 )
